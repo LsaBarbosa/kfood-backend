326ac9153375f184392eef9de5390413182f8717
LsaBarbosa <lsa94.contato@gmail.com>
2026-04-01 13:33:10 -0300

    corrigir política de criação de usuário do tenant

--- src/main/java/com/kfood/merchant/application/user/CreateMerchantUserUseCase.java
+++ src/main/java/com/kfood/merchant/application/user/CreateMerchantUserUseCase.java
@@ -9,8 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 public class CreateMerchantUserUseCase {
 
-  private static final Set<UserRoleName> ALLOWED_ROLES =
-      EnumSet.of(UserRoleName.MANAGER, UserRoleName.ATTENDANT);
+  private static final Set<UserRoleName> OWNER_ALLOWED_ROLES =
+      EnumSet.of(UserRoleName.OWNER, UserRoleName.MANAGER, UserRoleName.ATTENDANT);
+  private static final Set<UserRoleName> MANAGER_ALLOWED_ROLES = EnumSet.of(UserRoleName.ATTENDANT);
 
   private final MerchantUserManagementPort merchantUserManagementPort;
   private final MerchantTenantAccessPort merchantTenantAccessPort;
@@ -24,18 +25,43 @@ public class CreateMerchantUserUseCase {
 
   @Transactional
   public MerchantUserOutput execute(CreateMerchantUserCommand command) {
-    validateRoles(command.roles());
+    var actorRoles = normalizeRoles(merchantTenantAccessPort.getRequiredAuthenticatedUserRoles());
+    var requestedRoles = normalizeRoles(command.roles());
+
+    validateRoles(actorRoles, requestedRoles);
 
     var storeId = merchantTenantAccessPort.getRequiredStoreId();
     return merchantUserManagementPort.create(
-        storeId, command.email().trim(), command.password(), command.roles());
+        storeId, command.email().trim(), command.password(), requestedRoles);
   }
 
-  private void validateRoles(Set<UserRoleName> roles) {
-    for (var role : roles) {
-      if (!ALLOWED_ROLES.contains(role)) {
-        throw new InvalidMerchantUserRoleException(role);
-      }
+  private void validateRoles(Set<UserRoleName> actorRoles, Set<UserRoleName> requestedRoles) {
+    if (requestedRoles.size() != 1) {
+      throw new InvalidMerchantUserRoleException(requestedRoles);
     }
+
+    var requestedRole = requestedRoles.iterator().next();
+    if (!allowedRolesFor(actorRoles).contains(requestedRole)) {
+      throw new InvalidMerchantUserRoleException(requestedRole);
+    }
+  }
+
+  private Set<UserRoleName> allowedRolesFor(Set<UserRoleName> actorRoles) {
+    if (actorRoles.contains(UserRoleName.OWNER)) {
+      return OWNER_ALLOWED_ROLES;
+    }
+    if (actorRoles.contains(UserRoleName.MANAGER)) {
+      return MANAGER_ALLOWED_ROLES;
+    }
+
+    return EnumSet.noneOf(UserRoleName.class);
+  }
+
+  private EnumSet<UserRoleName> normalizeRoles(Set<UserRoleName> roles) {
+    var normalizedRoles = EnumSet.noneOf(UserRoleName.class);
+    if (roles != null) {
+      normalizedRoles.addAll(roles);
+    }
+    return normalizedRoles;
   }
 }
